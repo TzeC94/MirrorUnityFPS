04ef49ed134247f5908ccb6e70cf38e663830e35
Teoh Tze Chian <teoh_0827@hotmail.com>
2023-01-16 20:40:18 +0800

    Combined Health and Death into Combat

---
+++ Assets/Scripts/Combat/BaseCombatScript.cs
@@ -0,0 +1,88 @@
+using Mirror;
+using UnityEngine;
+using System;
+
+public class BaseCombatScript : NetworkBehaviour, IHitable {
+
+    [Header("Health")]
+    [SerializeField]
+    private int maxHealth = 100;
+    [SyncVar]
+    protected int currentHealth = 0;
+    public int CurrentHealth { get { return currentHealth; } }
+    public bool IsDead { get { return currentHealth <= 0f; } }
+
+    //Death
+    public Action OnDeathCallback;
+
+    // Start is called before the first frame update
+    private void Start() {
+
+        Initialize();
+
+    }
+
+    public virtual void Initialize() {
+
+        ResetHealth();
+
+    }
+
+    #region Health
+
+    public virtual void ResetHealth() {
+
+        if (isServer) {
+            currentHealth = maxHealth;
+        }
+
+    }
+
+    #endregion
+
+    #region Death
+
+    public virtual bool DeathCheck() {
+
+        return IsDead;
+
+    }
+
+    public virtual void OnKilled(HitInfo hitInfo) {
+
+        OnDeathCallback?.Invoke();
+        RPCOnKilled(hitInfo);
+
+    }
+
+    public virtual void Respawn() {
+
+        ResetHealth();
+
+    }
+
+    [ClientRpc]
+    public virtual void RPCOnKilled(HitInfo hitInfo) {
+
+
+
+    }
+
+    #endregion
+
+    public virtual void OnHit(HitInfo hitInfo) {
+
+        if (isServer) {
+
+            currentHealth -= hitInfo.damage;
+
+            if (DeathCheck()) {
+
+                OnKilled(hitInfo);
+
+            }
+
+        }
+
+    }
+}
